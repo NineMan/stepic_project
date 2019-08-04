c6806a588b56fc15fec517cc7687efc9a22844ee
NineMan <nelubinmv@inbox.ru>
2019-08-04 04:43:28 -0700

    first conf gunicorn

--- init.sh
+++ init.sh
@@ -1,3 +1,19 @@
+# setting for nginx
+
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+
+
+# settings for gunicorn
+
+sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
+
+# sudo gunicorn -b 0.0.0.0:8080 gunicorn hello:application
+
+cd /home/box/web
+sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
+
+
+# setting for mysql
 
