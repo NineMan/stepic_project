832624c1ac3904f72640f20362e1b563487a6480
NineMan <nelubinmv@inbox.ru>
2019-08-04 23:53:04 +0000

    second config gunicorn

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 # setting for nginx
 
+#sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
