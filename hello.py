103c5b959f0cf518bd778c26fc6b7f4d43da7071
NineMan <NineMan@github.com>
2019-09-16 03:56:00 +0400

    повторно шаг 1.9

--- hello.py
+++ hello.py
@@ -1,8 +1,22 @@
-def application(env, start_response):
+from cgi import parse_qs                                                        
 
-  print(env['QUERY_STRING'])
-  data = env['QUERY_STRING'].split('&')
-  data = '\n'.join(data)
-#  data = bytes(data, encoding='utf-8')
-  start_response('200 OK', [('Content-Type', 'text/plain')])
-  return iter([data])
+def application(environ, start_response):
+
+	queryString = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+	body = ''
+	print(queryString)
+	for key, value in queryString.items() :
+		for element in value :
+			body += key + '='                                       
+			body += element + '\r\n'
+
+	print(body)                                                             
+		
+	status = '200 OK'
+
+	response_headers = [
+		('Content-Type', 'text/plain'),
+	]
+	start_response(status, response_headers)                                    
+
+	return [body]
--- init.sh
+++ init.sh
@@ -9,30 +9,25 @@ sudo /etc/init.d/nginx restart
 
 # settings for gunicorn
 
-
 # ----------------- Ver 1 -------------------------
 # sudo ln -s       /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
 # cd /home/box/web
 # sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
 
-
 # ----------------- Ver 2 -------------------------
 # sudo gunicorn -b 0.0.0.0:8080 gunicorn hello:application
-
 
 # ----------------- Ver 3 -------------------------
 # cd /home/box/web
 # gunicorn --bind='0.0.0.0:8080' hello:application
 
-
 # ----------------- Ver 4 -------------------------
 # sudo ln -s       /home/box/web/etc/gunicorn1.py /etc/gunicorn.d/gunicorn.py
 # sudo gunicorn -c /home/box/web/etc/gunicorn1.py 
 
-
 # ----------------- Ver 5 -------------------------
 sudo rm -r /etc/gunicorn.d/*
-sudo ln -sf /home/box/web/etc/gunicorn2.py   /etc/gunicorn.d/gunicorn2.py
+sudo ln -sf /home/box/web/etc/gunicorn.py   /etc/gunicorn.d/gunicorn.py
 # sudo ln -sf /home/box/web/etc/qa.py   /etc/gunicorn.d/qa.py
 sudo /etc/init.d/gunicorn restart
 
