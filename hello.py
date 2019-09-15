ec5f70558ffb0061cb5b349c3295fc1963da2368
NineMan <53180114+NineMan@users.noreply.github.com>
2019-09-16 02:30:15 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,6 @@ def application(env, start_response):
   print(env['QUERY_STRING'])
   data = env['QUERY_STRING'].split('&')
   data = '\n'.join(data)
-  data = bytes(data, encoding='utf-8')
+#  data = bytes(data, encoding='utf-8')
   start_response('200 OK', [('Content-Type', 'text/plain')])
-  return [data]
-
+  return iter([data])
