e99a85982e2fe9d30b3406cadfd5245e182d9492
NineMan <NineMan@github.com>
2019-09-18 01:52:58 +0400

    2.1 ver 1

---
+++ ask/qa/urls.py
@@ -0,0 +1,16 @@
+# from django.contrib import admin
+# from django.urls import path
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('qa.views',
+    url(r'^$', 'index', name='index'),
+    url(r'^login/', 'login', name='login'),
+    url(r'^signup/', 'signup', name='signup'),
+    url(r'^question/(?P<quest_id>[0-9]+)/$', 'question', name='question'),
+    url(r'^ask/', 'ask', name='ask'),
+    url(r'^answer/', 'answer', name='answer'),
+    url(r'^popular/', 'popular', name='popular'),
+    url(r'^new/', 'new', name='new'),
+
+]
