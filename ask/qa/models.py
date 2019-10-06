b76b247adeaa8fb359c3488b1c04039cf1b253e3
NineMan <NineMan@github.com>
2019-10-07 03:42:27 +0400

    add (change) qa/models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    title = models.Charfield('name question', max_length = 100)
+    text = models.TextField('text question')
+    added_at = models.DateTmeField('date added question')
+    rating = models.IntegerField('rating of question')
+    author = models.Charfield('name author of question', max_length = 50)
+    likes = models.Integer('number of likes of question')
+
+
+class Answer(models.Model):
+    text = models.TextField('text answer')
+    added_at = models.DateTmeField('date added answer')
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    author = models.Charfield('name author of answer', max_length = 50)
+
