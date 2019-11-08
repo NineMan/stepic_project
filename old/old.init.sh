beb5d3fb20071f09b9f104d07a3470b503ce5e63
NineMan <NineMan@github.com>
2019-11-08 04:06:47 +0400

    correct init.sh and mv in .old/

--- old/old.init.sh
+++ old/old.init.sh
@@ -7,6 +7,7 @@ sudo /etc/init.d/nginx restart
 
 # settings for gunicorn
 
+# ----------------- Ver 5 -------------------------
 # --- for stepic ---
 # sudo rm -r /etc/gunicorn.d/*
 # sudo ln -sf /home/box/web/etc/gunicorn.py   /etc/gunicorn.d/gunicorn.py
@@ -14,7 +15,6 @@ sudo /etc/init.d/nginx restart
 # cd /home/box/web/ask
 # sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
 # sudo /etc/init.d/gunicorn restart
-
 # --- for local ---
 gunicorn -c /home/box/web/etc/gunicorn1.py hello:application 
 gunicorn -c /home/box/web/etc/qa1.py       ask.wsgi:application
@@ -22,19 +22,12 @@ gunicorn -c /home/box/web/etc/qa1.py       ask.wsgi:application
 
 # setting for mysql
 
-# --- for local ---
-
-
-
-# --- for all ---
  sudo /etc/init.d/mysql restart
  mysql -uroot -e "DROP DATABASE ASK"
  mysql -uroot -e "DROP USER sa@localhost"
  mysql -uroot -e "CREATE DATABASE ASK"
  mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
  mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
-# sudo service mysql stop
-
 
 # --- for stepic ---
 # python3 /home/box/web/ask/manage.py makemigrations qa
@@ -44,4 +37,25 @@ gunicorn -c /home/box/web/etc/qa1.py       ask.wsgi:application
  python /home/box/web/ask/manage.py makemigrations qa
  python /home/box/web/ask/manage.py migrate qa
 
+
+
+
+# ----------------- setting for gunicorn
+# ----------------- Ver 1 -------------------------
+# sudo ln -s       /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
+# cd /home/box/web
+# sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
+# ----------------- Ver 2 -------------------------
+# sudo gunicorn -b 0.0.0.0:8080 hello:application
+# ----------------- Ver 3 -------------------------
+# cd /home/box/web
+# gunicorn --bind='0.0.0.0:8080' hello:application
+# ----------------- Ver 4 -------------------------
+# sudo ln -s       /home/box/web/etc/gunicorn1.py /etc/gunicorn.d/gunicorn.py
+# sudo gunicorn -c /home/box/web/etc/gunicorn1.py
+# ----------------- Ver 5 -------------------------
+# sudo rm -r /etc/gunicorn.d/*
+# sudo ln -sf /home/box/web/etc/gunicorn.py   /etc/gunicorn.d/gunicorn.py
+# sudo ln -sf /home/box/web/etc/qa.py         /etc/gunicorn.d/qa.py
+# sudo /etc/init.d/gunicorn restart
 
