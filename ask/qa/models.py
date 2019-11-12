e95ae01c9a17d4e4ddb1ab90766c95c0e47d1298
NineMan <NineMan@github.com>
2019-11-13 03:38:40 +0400

    Step 2.5 Up 1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -11,17 +11,22 @@ class QuestionManager(models.Manager):
 
 class Question(models.Model):
     objects = QuestionManager()
-    title = models.CharField(max_length=100)
-    text = models.TextField()
+    title = models.CharField(max_length=100, default="")
+    text = models.TextField(default="")
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, related_name='question_author', on_delete=models.CASCADE)
     likes  = models.ManyToManyField(User, related_name='question_likes', blank=True)
+    class Meta:
+        db_table = "qa_questions" 
+    
 
 
 class Answer(models.Model):
-    text = models.TextField()
+    text = models.TextField(default="")
     added_at = models.DateTimeField(auto_now=True)
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     author = models.ForeignKey(User, related_name='answer_author', on_delete=models.CASCADE)
+    class Meta:
+        db_table = "qa_answers"
 
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import url
-from qa.views import test
+from qa.views import test, main, popular, question
 
 
 urlpatterns = (
-    url(r'^$', test, name='index'),
+    url(r'^$', main, name='index'),
     url(r'^login/', test, name='login'),
     url(r'^signup/', test, name='signup'),
-    url(r'^question/(?P<quest_id>[0-9]+)/$', test, name='question'),
+    url(r'^question/(?P<question_id>[0-9]+)/$', question, name='question'),
     url(r'^ask/', test, name='ask'),
     url(r'^answer/', test, name='answer'),
-    url(r'^popular/', test, name='popular'),
+    url(r'^popular/', popular, name='popular'),
     url(r'^new/', test, name='new'),
 )
 
