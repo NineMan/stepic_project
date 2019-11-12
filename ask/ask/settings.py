96557d340031fb920c44d2ca68e118552d9f1bbe
NineMan <NineMan@github.com>
2019-11-13 03:38:40 +0400

    Step 2.5 Up and Correct

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'ask.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '/home/box/web/ask/templates/',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,67 @@
-# from django.shortcuts import render
+# # from django.shortcuts import render
+# from django.http import HttpResponse
+# 
+# def test(request, *args, **kwargs):
+#     return HttpResponse('OK')
+# 
+
+
+
+from django.core.paginator import Paginator, EmptyPage
 from django.http import HttpResponse
+from django.shortcuts import render
+from qa.models import *
 
 def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+	return HttpResponse('OK')
 
+def main(request):
+	limit = 10
+	try:
+		page = int(request.GET.get("page", 1))
+	except ValueError:
+		page = 1
+	questions = Question.objects
+	questions = questions.order_by("-added_at")
+	paginator = Paginator(questions, limit)
+	paginator.baseurl = "/?page="
+	try:
+		page = paginator.page(page)
+	except EmptyPage:
+		page = paginator.page(paginator.num_pages)
+	return render(request, "main_page.html", {
+		'questions': page.object_list,
+		'paginator': paginator, 
+		'page': page,
+	})
+
+def popular(request):
+	limit = 10
+	try:
+		page = int(request.GET.get('page', 1))
+	except ValueError:
+		page = 1
+	questions = Question.objects.order_by("-rating")
+	paginator = Paginator(questions, limit)
+	paginator.baseurl = "/popular/?page="
+	try:
+		page = paginator.page(page)
+	except EmptyPage:
+		page = paginator.page(paginator.num_pages)
+	return render(request, "popular_page.html", {
+		'questions': page.object_list,
+		'paginator': paginator,
+		'page': page,
+	})
+
+def question(request, question_id):
+	try:
+		question_id = int(question_id)
+	except ValueError:
+		raise Http404
+	q = Question.objects.get(pk=question_id)
+	answers = Answer.objects.all().filter(question = q)
+	return render(request, "question_page.html", {
+		'question': q,
+		'answers': answers,
+	})
